813fac24c09dd4d8509b91f7c453cb790e5cb938
fedespl <federicoperezlinares@gmail.com>
2023-09-17 20:14:16 +0200

    add task form type

---
+++ src/Form/TaskFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class TaskFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Task Name',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255]),
+                ],
+                'attr' => [
+                    'name' => 'task_name'
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
